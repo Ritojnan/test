414758adc1ce17f0b10eb0292170adcffe956baf
Prop-Art <91047507+Prop-Art@users.noreply.github.com>
2021-09-20 12:11:00 +0530

    Add files via upload

---
+++ backdrops.js
@@ -0,0 +1,15 @@
+
+
+
+document.getElementById("albumimage1").src="assets/images/displayimg/Slide38.JPG"
+document.getElementById("albumimage2").src="assets/images/displayimg/Slide37.JPG"
+document.getElementById("albumimage3").src="assets/images/displayimg/Slide39.JPG"
+document.getElementById("albumimage4").src="assets/images/displayimg/Slide37.JPG"
+document.getElementById("albumimage5").src="assets/images/displayimg/Slide37.JPG"
+document.getElementById("albumimage6").src="assets/images/displayimg/Slide37.JPG"
+document.getElementById("albumimage7").src="assets/images/displayimg/Slide37.JPG"
+document.getElementById("albumimage8").src="assets/images/displayimg/Slide37.JPG"
+document.getElementById("albumimage9").src="assets/images/displayimg/Slide37.JPG"
+document.getElementById("albumimage10").src="assets/images/displayimg/Slide37.JPG"
+
+document.getElementById("bestseller").src="assets/images/displayimg/Slide37.JPG"
