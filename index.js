03fbce9427defcb055898c4c7fb08b3e8f0f3068
Prop-Art <91047507+Prop-Art@users.noreply.github.com>
2021-09-20 12:06:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("albumimagei").src="assets/images/displaypanel/Slide1.JPG"
+document.getElementById("albumimageii").src="assets/images/displaypanel/Slide2.JPG"
+document.getElementById("albumimageiii").src="assets/images/displaypanel/Slide3.JPG"
+
+
